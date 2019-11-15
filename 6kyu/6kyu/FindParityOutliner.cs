89650867271b959cf32a51d2fafb3fb55007cf24
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2019-11-16 07:36:28 +0800

    Find Parity Outliner

---
+++ 6kyu/6kyu/FindParityOutliner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace _6kyu
+{
+
+    public class FindParityOutliner
+    {
+        public static int Find(int[] integers)
+        {
+            var evenNumbers = integers.Where(integer => integer % 2 == 0);
+            var oddNumbers = integers.Where(integer => integer % 2 == 1);
+            return evenNumbers.Count() == 1 ? evenNumbers.First() : oddNumbers.First();
+        }
+    }
+}
